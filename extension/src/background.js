33cc017ca38969f94326f10407a7f31efb6459ef
Abhishek Batra <abhshk.btra@gmail.com>
2021-11-05 20:05:47 +0530

    refactor: remove boilerplate from background.js

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,26 +1,10 @@
 'use strict';
 import { DomainActivatedHandler, TabActivatedHandler } from './handlers';
-import { OffersLoader } from './offers';
 
 // With background scripts you can communicate with popup
 // and contentScript files.
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
-
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
-  }
-});
 
 chrome.webNavigation.onBeforeNavigate.addListener(function(e) {
   const domainActivatedHandler = new DomainActivatedHandler();
